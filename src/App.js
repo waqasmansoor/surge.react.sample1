e53307ba042e57a4b1eb62371bf651fdf5ad88e6
Waqas Mansoor <waqasmansoor47@yahoo.com>
2020-06-15 18:26:25 +0500

    modified index.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world from waqas
+          Hello world from waqas with auto deploy
         </p>
         <a
           className="App-link"
